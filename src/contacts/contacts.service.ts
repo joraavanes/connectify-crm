87f142bf71ceb5680558b3ebb392388655a3051c
Jora <jora_a@outlook.com>
2023-03-05 22:35:59 +0330

    feat: add CRUD operation for contact model

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -2,30 +2,52 @@ import { Injectable } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 import { Contact } from './domain/contact.entity';
 import { CreateContactDto } from './dtos/create-contact.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ContactsService {
-  private contacts: Contact[];
+  constructor(
+    @InjectRepository(Contact) private repo: Repository<Contact>
+  ) { }
 
-  constructor() {
-    this.contacts = Array(5).fill(undefined).map(() => ({
-      id: faker.datatype.number(),
-      fullname: faker.name.fullName(),
-      email: faker.internet.email(),
-      mobile: faker.phone.number(),
-      phone: faker.phone.number(),
-      role: faker.company.catchPhraseNoun()
-    }));
-  }
-
-  createContact(contactDto: CreateContactDto) {
-    this.contacts.push({
-      id: faker.datatype.number(),
-      ...contactDto
-    });
+  createContact(model: CreateContactDto) {
+    const contact = this.repo.create({ ...model });
+    return this.repo.save(contact);
   }
 
   findContacts() {
-    return this.contacts;
+    return this.repo.find();
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOneBy({
+      email
+    })
+  }
+
+  async updateContact(id: number, attrs: Partial<Contact>) {
+    const model = await this.repo.findOneBy({
+      id
+    });
+
+    if (!model) return undefined;
+
+    const updatedContact: Contact = {
+      ...model,
+      ...attrs
+    };
+
+    return this.repo.save(updatedContact);
+  }
+
+  async removeContact(id: number) {
+    const model = await this.repo.findOneBy({
+      id
+    });
+
+    if(!model) return undefined;
+
+    return this.repo.remove(model);
   }
 }
