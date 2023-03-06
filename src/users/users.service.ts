bdfef4b015dc2d02c8ae61b47dc34a2d3bf3aa82
Jora <jora_a@outlook.com>
2023-03-07 01:20:53 +0330

    feat: CRUD operation for user entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './domain/user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private repo: Repository<User>
+  ) { }
+
+  async createUser(dto: CreateUserDto) {
+    const model = await this.findByEmail(dto.email);
+    if (model)
+      return false;
+
+    const contact = this.repo.create(dto);
+    return this.repo.save(contact);
+  }
+
+  async updateUser(id: number, attrs: Partial<User>) {
+    const user = await this.findById(id);
+    if (!user) return false;
+
+    const updatedUser = {
+      ...user,
+      ...attrs
+    }
+
+    return this.repo.save(updatedUser);
+  }
+
+  async removeUser(id: number) {
+    const user = await this.findById(id);
+    if (!user) return false;
+
+    return this.repo.remove(user);
+  }
+
+  findUsers() {
+    return this.repo.findBy({});
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOneBy({ email });
+  }
+
+  findById(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+}
