8c345b48938d76e6d2fc0cd8247eef8a19355c7f
Jora <jora_a@outlook.com>
2023-03-07 01:27:34 +0330

    feat: utilize usersService on the endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dtos';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  @Get()
+  findUsers() {
+    return this.usersService.findUsers();
+  }
+
+  @Get(':email')
+  async findUser(@Param('email') email: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Post()
+  async createUser(@Body() body: CreateUserDto) {
+    const model = await this.usersService.createUser(body);
+
+    if (!model) throw new BadRequestException();
+
+    return model;
+  }
+
+  @Patch(':id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const model = await this.usersService.updateUser(parseInt(id), body);
+
+    if (!model) throw new BadRequestException();
+
+    return model;
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    const model = await this.usersService.removeUser(parseInt(id));
+    if (!model) throw new NotFoundException();
+    return model;
+  }
+}
