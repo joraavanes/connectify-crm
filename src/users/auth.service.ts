5597a02b489e545807386c61e20e78a9fbfcf8cd
Jora <jora_a@outlook.com>
2023-03-08 14:28:53 +0330

    feat: auth service with signup logic in Users module

---
+++ src/users/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _script } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_script);
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  async singup(dto: CreateUserDto) {
+    const user = await this.usersService.findByEmail(dto.email);
+    if (user) return false;
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = await scrypt(dto.password, salt, 32) as Buffer;
+
+    const result = `${hash}.${salt}`;
+
+    return this.usersService.createUser({
+      ...dto,
+      password: result
+    });
+  }
+
+  singin() { }
+}
