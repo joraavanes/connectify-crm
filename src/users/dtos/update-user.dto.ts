64566c8b6a43c865aaf32bf55841b3c076c93b07
Jora <jora_a@outlook.com>
2023-03-08 17:20:39 +0330

    fix: remove email on user upate dto

--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -2,10 +2,6 @@ import { IsEmail, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
   @IsOptional()
-  @IsEmail()
-  email: string;
-  
-  @IsOptional()
   @IsString()
   password: string;
   
