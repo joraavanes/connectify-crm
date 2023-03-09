57760c614828966e55c5bda0df62e0653ccb1451
Jora <jora_a@outlook.com>
2023-03-09 18:01:26 +0330

    Add cookie-session middleware to the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+const cookieSession = require('cookie-session');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    secret: 'SUPERSECRETKEY@2023#now'
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
