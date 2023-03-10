593b573fa1784168a1b2678a9ebe4ccf55bb27d6
Jora <jora_a@outlook.com>
2023-03-10 13:10:43 +0330

    feat: add object serializer for outgoing responses

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,28 @@
+import { ExecutionContext, Injectable, NestInterceptor, CallHandler, UseInterceptors } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { Observable, map } from 'rxjs';
+
+interface ClassConstructor {
+  new(...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+@Injectable()
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(
+    private dto: ClassConstructor
+  ) { }
+
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true
+        });
+      })
+    );
+  }
+}
