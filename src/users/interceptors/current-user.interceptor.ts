0c8ab09d0b359b4b8752bcdbe7f2c04ac612675f
Jora <jora_a@outlook.com>
2023-03-12 00:33:19 +0330

    feat: create currentUser decorator

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,22 @@
+import { NestInterceptor, ExecutionContext, CallHandler, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(
+    private usersService: UsersService
+  ) { }
+
+  async intercept(ctx: ExecutionContext, next: CallHandler<any>) {
+    const request = ctx.switchToHttp().getRequest();
+    const { userId } = request.session;
+
+    if (userId) {
+      const user = await this.usersService.findById(userId);
+      request.currentUser = user;
+    }
+
+    return next.handle();
+  }
+}
