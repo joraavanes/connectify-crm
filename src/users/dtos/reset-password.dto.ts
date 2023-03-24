af1d3062c57ad7408772b7b2509e9efbf22d6af5
Jora <jora_a@outlook.com>
2023-03-25 01:20:05 +0430

    add custom message on password confirmation

--- src/users/dtos/reset-password.dto.ts
+++ src/users/dtos/reset-password.dto.ts
@@ -4,14 +4,14 @@ import { Match as MatchWith } from "../../CustomValidators/MatchWith";
 export class ResetPasswordDto {
   @IsEmail()
   email: string;
-  
+
   @IsString()
   currentPassword: string;
-  
+
   @IsString({ message: '' })
-  @MatchWith(ResetPasswordDto, o => o.passwordConfirm)
+  @MatchWith(ResetPasswordDto, o => o.passwordConfirm, { message: 'Passwords don\'t match' })
   newPassword: string;
-  
+
   @IsString()
   passwordConfirm: string;
 }
