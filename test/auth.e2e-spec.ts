d6987b531f04d3860048c082ac8c0d3ef51436ad
Jora <jora_a@outlook.com>
2023-04-01 18:49:24 +0430

    test: get data on /users/current-user after sign up

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -22,17 +22,44 @@ describe('Auth e2e test', () => {
     role: 'CTO'
   };
 
-  it('should sign up a new user', () => {
+  it('should sign up a new user', async () => {
     return request(app.getHttpServer())
       .post('/users/signup')
       .send(model1)
       .expect(201)
       .then(res => {
         const { email, fullname, role } = res.body;
-        
+
         expect(email).toEqual(model1.email);
         expect(fullname).toEqual(model1.fullname);
         expect(role).toEqual(model1.role);
       });
   });
+
+  it('should sign up a new user and get registered user info from /users/current-user', async () => {
+    let cookie: string[];
+
+    await request(app.getHttpServer())
+      .post('/users/signup')
+      .send(model1)
+      .expect(201)
+      .expect(res => {
+        cookie = res.get('Set-Cookie');
+      });
+
+    return request(app.getHttpServer())
+      .get('/users/current-user')
+      .set('Cookie', cookie)
+      .expect(200)
+      .expect(res => {
+        const { email } = res.body;
+        expect(email).toBe(model1.email);
+      });
+  });
+
+  afterEach(() => {
+    return request(app.getHttpServer())
+      .delete(`/users/${model1.email}`);
+    // .expect(200);
+  })
 });
