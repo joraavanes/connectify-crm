e2158d16266dc4f9b627540131bec26658ee4021
Jora <jora_a@outlook.com>
2023-04-02 21:09:55 +0430

    feat: add creating inqiury on service, typeorm entity setup

--- src/inquiries/inquiries.module.ts
+++ src/inquiries/inquiries.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { InquiriesService } from './inquiries.service';
 import { InquiriesController } from './inquiries.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Inquiry } from './domain/inquiry.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Inquiry])],
   providers: [InquiriesService],
   controllers: [InquiriesController]
 })
-export class InquiriesModule {}
+export class InquiriesModule { }
--- src/inquiries/inquiries.service.ts
+++ src/inquiries/inquiries.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Inquiry } from './domain/inquiry.entity';
+import { User } from 'src/users/domain/user.entity';
+import { CreateInquiryDto } from './dtos';
 
 @Injectable()
-export class InquiriesService {}
+export class InquiriesService {
+  constructor(
+    @InjectRepository(Inquiry) private repo: Repository<Inquiry>
+  ) { }
+
+  async createInquiry(dto: CreateInquiryDto, currentUser: User) {
+    const inquiry = await this.repo.create(dto);
+    inquiry.user = currentUser;
+
+    return this.repo.save(inquiry);
+  }
+}
