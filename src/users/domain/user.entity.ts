4304cb0f9ad584483e264ac294bcaa8f2f4182bf
Jora <jora_a@outlook.com>
2023-04-02 18:10:05 +0430

    fix: correct domain dir

--- src/users/domain/user.entity.ts
+++ src/users/domain/user.entity.ts
@@ -1,4 +1,4 @@
-import { Inquiry } from "src/inquiries/doamin/inquiry.entity";
+import { Inquiry } from "src/inquiries/domain/inquiry.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
