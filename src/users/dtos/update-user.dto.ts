f985fc66ebb126d030d54d2abd8f767bee25d084
Jora <jora_a@outlook.com>
2023-04-02 12:12:02 +0430

    fix: remove password from UpdateUserDto

--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -3,10 +3,6 @@ import { IsEmail, IsOptional, IsString } from "class-validator";
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
-  password: string;
-  
-  @IsOptional()
-  @IsString()
   fullname: string;
   
   @IsOptional()
