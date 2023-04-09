e24da85a6f680f66d4a36dad531a81bddbe671e7
Jora <jora_a@outlook.com>
2023-04-09 17:16:54 +0430

    feat: add authorization check to authGuard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,9 +1,19 @@
 import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Request } from "express";
 import { Observable } from "rxjs";
 
 export class AuthGuard implements CanActivate {
+  private roles: string[];
+  constructor(...roles: string[]) {
+    this.roles = roles;
+  }
+
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    return request.session.userId;
+    const request = context.switchToHttp().getRequest<Request>();
+
+    if (!request?.session?.userId) return false;
+
+    return this.roles.length ?
+      this.roles.some(role => role === request?.currentUser?.role) : true;
   }
 }
