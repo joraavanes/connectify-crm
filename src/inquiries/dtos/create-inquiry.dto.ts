ff11d5cdfc4172ee94dfc8a893c25ea9b6a4bca9
Jora <jora_a@outlook.com>
2023-04-16 20:37:18 +0430

    fix: set hours to 0 on inquiry's date

--- src/inquiries/dtos/create-inquiry.dto.ts
+++ src/inquiries/dtos/create-inquiry.dto.ts
@@ -2,7 +2,9 @@ import { IsDate, IsString } from "class-validator";
 
 export class CreateInquiryDto {
   constructor() {
-    this.issuedAt = new Date();
+    const issuedAt = new Date();
+    issuedAt.setHours(0, 0, 0, 0);
+    this.issuedAt = issuedAt;
   }
 
   @IsString()
