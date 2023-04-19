d512b324f01f7488268eb66bcaaaffffb74e18e7
Jora <jora_a@outlook.com>
2023-04-19 22:17:38 +0430

    add count & pageNumber to the query dto

--- src/inquiries/dtos/query-inquiries.dto.ts
+++ src/inquiries/dtos/query-inquiries.dto.ts
@@ -1,24 +1,32 @@
 import { Transform } from "class-transformer";
-import { IsBoolean, IsDate, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsDate, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class QueryInquiriesDto {
   @IsString()
   @IsOptional()
   client: string;
-  
+
   @IsString()
   @IsOptional()
   product: string;
-  
+
   @IsOptional()
   @Transform(({ value }) => {
     return value ? new Date(value) : null;
   })
   @IsDate()
   issuedAt: Date;
-  
+
   @Transform(({ value }) => Boolean(+value))
   @IsBoolean()
   @IsOptional()
   user: boolean;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  count: number;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  pageNumber: number;
 }
