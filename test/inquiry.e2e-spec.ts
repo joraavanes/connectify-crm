af2461410b42cb7d6ed8997c6f1e0d9724889a37
Jora <jora_a@outlook.com>
2023-04-25 10:50:59 +0330

    add unit tests for update, create endpoints

--- test/inquiry.e2e-spec.ts
+++ test/inquiry.e2e-spec.ts
@@ -21,6 +21,7 @@ describe('Inquiry e2e test', () => {
   };
 
   beforeEach(async () => {
+    inquiryId = null;
     const module = await Test.createTestingModule({
       imports: [AppModule]
     }).compile();
@@ -47,10 +48,46 @@ describe('Inquiry e2e test', () => {
       });
   });
 
-  afterEach(async () => {
+  it('fails to create inquiry without user id (Cookie)', async () => {
+    return request(app.getHttpServer())
+      .post('/inquiries')
+      .send(inquiryModel)
+      .expect(403);
+  });
+
+  it('should update an inquiry successfully', async () => {
     await request(app.getHttpServer())
-      .delete(`/inquiries/${inquiryId}`)
+      .post('/inquiries')
+      .set('Cookie', cookie)
+      .send(inquiryModel)
+      .expect(res => { inquiryId = res.body.id });
+      
+      return request(app.getHttpServer())
+      .patch(`/inquiries/${inquiryId}`)
+      .set('Cookie', cookie)
+      .send({ product: 'stamps' })
       .expect(200);
+  });
+
+  it('should fail to update an inquiry without user id (Cookie)', async () => {
+    await request(app.getHttpServer())
+      .post('/inquiries')
+      .set('Cookie', cookie)
+      .send(inquiryModel)
+      .expect(res => { inquiryId = res.body.id });
+
+    return request(app.getHttpServer())
+      .patch(`/inquiries/${inquiryId}`)
+      .send({ product: 'stamps' })
+      .expect(403);
+  });
+
+  afterEach(async () => {
+    if (inquiryId) {
+      await request(app.getHttpServer())
+        .delete(`/inquiries/${inquiryId}`)
+        .expect(200);
+    }
 
     await request(app.getHttpServer())
       .delete(`/users/${userModel.email}`)
