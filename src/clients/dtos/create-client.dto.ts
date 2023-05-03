883d82b72d162fc72f8e662f434cd2ca8f7e3e10
Jora <jora_a@outlook.com>
2023-05-03 18:17:20 +0430

    add dto for creating client

---
+++ src/clients/dtos/create-client.dto.ts
@@ -0,0 +1,41 @@
+import {
+    IsEmail,
+    IsOptional,
+    IsString,
+    MaxLength
+} from "class-validator";
+
+export class CreateClientDto {
+    @IsString()
+    @MaxLength(50)
+    name: string;
+
+    @IsString()
+    @MaxLength(50)
+    industry: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(50)
+    country: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(50)
+    city: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(240)
+    postalAddress: string;
+
+    @IsEmail()
+    @IsOptional()
+    @MaxLength(240)
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(15)
+    phone: string;
+}
