4f540117f6a626810ca17cb4093f9e686c4a74e0
Jora <jora_a@outlook.com>
2023-05-03 19:31:29 +0430

    feat: add basic CRUD operation on Client

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Client } from './domain/client.entity';
+import { CreateClientDto } from './dtos/create-client.dto';
+import { User } from 'src/users/domain/user.entity';
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+    constructor(
+        @InjectRepository(Client) private repo: Repository<Client>
+    ) { }
+
+    findClient(id: number) {
+        return this.repo.findOneBy({ id });
+    }
+
+    findClients() {
+        return this.repo.find({
+            order: {
+                issuedAt: { direction: 'DESC' },
+                id: { direction: 'DESC' }
+            }
+        })
+    }
+
+    createClient(dto: CreateClientDto, user: User) {
+        const client = this.repo.create(dto);
+        client.user = user;
+
+        return this.repo.save(client);
+    }
+
+    async updateClient(id: number, attrs: Partial<Omit<Client, 'id'>>) {
+        const client = await this.findClient(id);
+        if (!client) return undefined;
+
+        const updatedClient: Partial<User> = {
+            ...client,
+            ...attrs
+        };
+
+        return this.repo.save(updatedClient);
+    }
+
+    async deleteClient(id: number) {
+        const client = await this.findClient(id);
+        if (!client) return undefined;
+
+        return this.repo.remove(client);
+    }
+}
