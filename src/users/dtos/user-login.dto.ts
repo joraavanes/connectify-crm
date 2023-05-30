1955d53a4597c74fb53a059c3d6ac960c04ff8d0
Jora <jora_a@outlook.com>
2023-05-30 15:34:41 +0430

    fix: IsNotEmpty validator on email prop

--- src/users/dtos/user-login.dto.ts
+++ src/users/dtos/user-login.dto.ts
@@ -2,6 +2,7 @@ import { IsEmail, IsString, IsNotEmpty } from "class-validator";
 
 export class UserLoginDto {
   @IsEmail({}, { message: 'Email is not valid' })
+  @IsNotEmpty({ message: 'Please enter your email' })
   email: string;
 
   @IsString()
